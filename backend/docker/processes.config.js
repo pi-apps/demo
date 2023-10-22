09466ff3df9a0d036459941337e8f654ff5ce6b1
arifendryw999 <arifendryw777@gmail.com>
2023-10-22 14:15:07 +0700

    whd-pi

--- backend/docker/processes.config.js
+++ backend/docker/processes.config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   apps: [{
-    name: "demoapp-backend",
+    name: "my-testnet.whd-pi-app-backend",
     script: "/usr/src/app/build/index.js",
     exec_mode: "cluster",
     instances: 4,
