a326626e05713c4888ca377e71733115b3a3066b
Alihan KOÇ <alhnkc@gmail.com>
2025-02-26 14:35:53 +0300

    fix: Removed the Cors header from the axios config because it is a server-side header and sending it from the frontend has no effect.

--- frontend/src/Shop/index.tsx
+++ frontend/src/Shop/index.tsx
@@ -50,7 +50,7 @@ const _window: WindowWithEnv = window;
 const backendURL = _window.__ENV && _window.__ENV.backendURL;
 
 const axiosClient = axios.create({ baseURL: `${backendURL}`, timeout: 20000, withCredentials: true});
-const config = {headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}};
+const config = {headers: {'Content-Type': 'application/json'}};
 
 
 export default function Shop() {
