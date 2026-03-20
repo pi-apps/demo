210cdf484fe4759e36ca9c7866201793bdbbf577
Hakkyung Lee <17398672+hklee93@users.noreply.github.com>
2026-03-21 01:48:06 +0900

    [Fix] checking prod via env.PROD

--- frontend/src/utils/redirectToLaunchpad.ts
+++ frontend/src/utils/redirectToLaunchpad.ts
@@ -1,7 +1,7 @@
 export const redirectToLaunchpad = () => {
   const env = import.meta.env;
 
-  if (env.NODE_ENV === "production") {
+  if (env.PROD) {
     window.open("launchpad.pinet.com", "_top", "noreferrer");
   } else {
     window.open("https://minepi.com", "_top", "noreferrer");
